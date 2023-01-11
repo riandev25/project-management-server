7602d8b83a08e0e2c6663ce27288e55f3fd931a0
Rian Miguel Engracia <engraciarian@gmail.com>
2023-01-11 20:20:18 +0800

    added cors header middleware

--- .history/src/utils/createApp_20230111194846.ts
+++ .history/src/utils/createApp_20230111194846.ts
@@ -19,8 +19,8 @@ export const createApp = (): Express => {
 
   // Enable cors
   const corsOptions = {
-    origin: ['http://localhost:3000'],
-    credentials: true,
+    origin: 'http://localhost:3000',
+    credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200,
   };
   app.use(cors(corsOptions));

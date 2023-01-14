f747827686b1716a6593d89b187702578a07251d
Rian Miguel Engracia <engraciarian@gmail.com>
2023-01-14 14:03:32 +0800

    fixed error in login route

--- dist/routes/auth.route.js
+++ dist/routes/auth.route.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,13 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const passport_1 = __importDefault(require("passport"));
 const auth_controller_1 = require("../controllers/auth/auth.controller");
-const express_async_handler_1 = __importDefault(require("express-async-handler"));
 const router = (0, express_1.Router)();
 // router.use(connectToSessions);
-router.post('/login', (0, express_async_handler_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const result = yield passport_1.default.authenticate('local');
-    res.status(200).send({ status: 'success', message: result });
-})));
+router.post('/login', passport_1.default.authenticate('local'), (req, res) => {
+    res
+        .status(200)
+        .send({ status: 'success', message: 'Logged in successfully' });
+});
 router.post('/logout', (req, res, next) => {
     req.session.destroy((err) => {
         if (err)

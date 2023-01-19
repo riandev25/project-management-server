200a1305674f605f06de515d9dc01130312957ed
Rian Miguel Engracia <engraciarian@gmail.com>
2023-01-19 20:03:06 +0800

    fixed error in cookie

--- dist/utils/createApp.js
+++ dist/utils/createApp.js
@@ -70,7 +70,6 @@ const createApp = () => {
             saveUninitialized: false,
             cookie: {
                 sameSite: 'none',
-                domain: '',
                 httpOnly: true,
                 secure: true,
                 maxAge: 30 * 24 * 60 * 60 * 1000,
--- src/utils/createApp.ts
+++ src/utils/createApp.ts
@@ -57,7 +57,6 @@ export const createApp = (): Express => {
         saveUninitialized: false,
         cookie: {
           sameSite: 'none',
-          domain: '',
           httpOnly: true,
           secure: true,
           maxAge: 30 * 24 * 60 * 60 * 1000,
